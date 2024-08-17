33cd0e047f40484216fb66dfea709261183ad462
Welby Seely <welbyseely@gmail.com>
2024-08-17 11:08:53 -0400

    quick and dirty memory logs

--- test_minitron.py
+++ test_minitron.py
@@ -6,6 +6,14 @@ from transformers import AutoTokenizer, LlamaForCausalLM
 logging.basicConfig(level=logging.INFO, force=True)
 logger = logging.getLogger(__name__)
 
+# before
+allocated_memory = torch.cuda.memory_allocated(device='cuda')
+print(f"BEFORE Allocated Memory: {allocated_memory / 1024**2:.2f} MB")
+
+# Check reserved memory
+reserved_memory = torch.cuda.memory_reserved(device='cuda')
+print(f"BEFORE Reserved Memory: {reserved_memory / 1024**2:.2f} MB")
+
 # Load the tokenizer and model
 model_path = "nvidia/Llama-3.1-Minitron-4B-Width-Base"
 # model_path = "meta-llama/Meta-Llama-3-8B"
@@ -18,6 +26,14 @@ model = LlamaForCausalLM.from_pretrained(model_path, torch_dtype=dtype, device_m
 # Prepare the input text
 prompt = "The Open-Closed Principle is"
 inputs = tokenizer.encode(prompt, return_tensors='pt').to(model.device)
+
+# loaded
+allocated_memory = torch.cuda.memory_allocated(device='cuda')
+print(f"LOADED Allocated Memory: {allocated_memory / 1024**2:.2f} MB")
+
+# Check reserved memory
+reserved_memory = torch.cuda.memory_reserved(device='cuda')
+print(f"LOADED Reserved Memory: {reserved_memory / 1024**2:.2f} MB")
 
 # Generate the output
 before_ts_ms = int(time.time() * 1000)
