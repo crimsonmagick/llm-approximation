c1b1d5b1a53dcceab0054ec2e6d3fa9fef0f319d
Welby Seely <welbyseely@gmail.com>
2024-11-04 14:42:20 -0500

    Cloning works

--- llama/pruning/attention_pruning.py
+++ llama/pruning/attention_pruning.py
@@ -16,6 +16,7 @@ class LlamaAttentionPruner:
     def __init__(self, model: LlamaForCausalLM):
         self.model = model
     
+    @torch.no_grad()
     def prune(self, layer_idx):
         llama_attention = self.model.model.layers[layer_idx].self_attn
         if not isinstance(llama_attention, LlamaSdpaAttention):
@@ -25,19 +26,10 @@ class LlamaAttentionPruner:
         
         config, layer_idx = llama_attention.config, llama_attention.layer_idx
         pruned_llama_attention = PrunedLlamaSdpaAttention(config, layer_idx)
-        
-        params = chain(zip(llama_attention.q_proj.parameters(), pruned_llama_attention.q_proj.parameters()),
-                       zip(llama_attention.k_proj.parameters(), pruned_llama_attention.k_proj.parameters()),
-                       zip(llama_attention.v_proj.parameters(), pruned_llama_attention.v_proj.parameters()))
+        pruned_llama_attention.eval()
         pruned_llama_attention.to(llama_attention.q_proj.weight.device, dtype=llama_attention.q_proj.weight.dtype)
         
-        with torch.no_grad():
-            # pruned_llama_attention.q_proj.weight.copy_(llama_attention.q_proj.weight)
-            # pruned_llama_attention.k_proj.weight.copy_(llama_attention.k_proj.weight)
-            # pruned_llama_attention.v_proj.weight.copy_(llama_attention.v_proj.weight)
-            for param, copied_param in params:
-                copied_param.copy_(param)
-            self.model.model.layers[layer_idx].self_attn = pruned_llama_attention
-            
+        pruned_llama_attention.load_state_dict(llama_attention.state_dict())
+        self.model.model.layers[layer_idx].self_attn = pruned_llama_attention
+        
         print(self.model)
-        # logger.info(f"hi, layer_idx={layer_idx}")
