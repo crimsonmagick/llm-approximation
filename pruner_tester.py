3c04a981465ca4ee24027c12ddaa75b4427aa840
Welby Seely <welbyseely@gmail.com>
2024-11-18 02:03:39 -0500

    better prompt

--- pruner_tester.py
+++ pruner_tester.py
@@ -4,12 +4,13 @@ from transformers import AutoTokenizer, LlamaForCausalLM
 import torch
 
 model_name = 'meta-llama/Meta-Llama-3-8B'
-torch.manual_seed(633)
+# torch.manual_seed(633)
 model = LlamaForCausalLM.from_pretrained(model_name, torch_dtype=torch.bfloat16, device_map='cuda')
 print(model)
 pruner = LlamaModelPruner(model)
 
-pruner.prune_heads( {25: [19]})
+pruner.prune_heads({0: [1, 6, 12, 16, 31], 3: [2, 6], 4: [5, 9], 25: [19]})
+# pruner.prune_heads( {1: list(range(0, 26)), 3: [2, 6], 4: [5, 9], 25: [19]})
 # pruner.prune_layers([16, 17])
 # pruner.prune_heads( {0: list(range(0, 32))})
 print('-------------------------------------------------------------------')
@@ -19,7 +20,9 @@ print('-------------------------------------------------------------------')
 print(model)
 
 tokenize = AutoTokenizer.from_pretrained(model_name, use_fast=True)
-prompt = "The Mask of Zorro (1998 film) summary: "
+prompt = ("[SYSTEM] You are a film summary assistant. Briefly Provide the names of the main characters and the director of the moving. Summarize the plot of the movie in great detail.[/SYSTEM]"
+          "[USER] Provide a summary to The Mask of Zorro (1998 film)[/USER]"
+          "[ASSISTANT]")
 tokens = tokenize(prompt, return_tensors='pt')
 input_ids = tokens["input_ids"].to(model.device)
 attention_mask = tokens["attention_mask"].to(model.device)
@@ -28,7 +31,7 @@ evaluation = model.generate(
     attention_mask=attention_mask,
     do_sample=True,
     top_k=0,
-    max_length=500,
+    max_length=2500,
     top_p=0.9,
     temperature=0.85,
     repetition_penalty=1.1
