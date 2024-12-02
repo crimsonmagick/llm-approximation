250a714952c638d932dd980e5a8618b08f56ecfe
Welby Seely <welbyseely@gmail.com>
2024-12-01 23:55:45 -0500

    prepared code for additional plots

--- src/reporting/graphing.py
+++ src/reporting/graphing.py
@@ -15,21 +15,34 @@ if __name__ == '__main__':
         help='Input path for csv file, required'
     )
     args = parser.parse_args()
-    
+    # label,layer_idx,head_idxs,perplexity,average_energy_per_token_mj,average_time_per_token_ms,allocated_memory
     with open(args.input_path, mode='r') as file:
         reader = csv.DictReader(file)
+        
         baseline = next(reader)
         baseline_perplexity = float(baseline['perplexity'])
+        baseline_energy = float(baseline['average_energy_per_token_mj'])
+        test_time = float(baseline['average_time_per_token_ms'])
+        test_memory = float(baseline['allocated_memory'])
+        
         perplexity_sum_by_layer = dict()
+        energy_sum_by_layer = dict()
         perplexities = []
+        energy_per_token = []
         for row in reader:
             layer_idx = row['layer_idx']
+            test_perplexity = float(row['perplexity'])
+            test_energy = float(row['average_energy_per_token_mj'])
+            test_time = float(row['average_time_per_token_ms'])
+            test_memory = float(row['allocated_memory'])
+            
             if layer_idx in perplexity_sum_by_layer:
-                perplexity_sum_by_layer[layer_idx] = (perplexity_sum_by_layer[layer_idx] + float(row['perplexity'])) / 2
+                perplexity_sum_by_layer[layer_idx] = (perplexity_sum_by_layer[layer_idx] + test_perplexity) / 2
             else:
                 perplexity_sum_by_layer[layer_idx] = float(row['perplexity'])
         for p in perplexity_sum_by_layer.values():
-            perplexities.append((baseline_perplexity / p ) * 100)
+            perplexities.append((baseline_perplexity / p) * 100)
+        
         plt.plot(perplexities)
         plt.title("Per Layer Accuracy After Attention Head Pruning")
         plt.xlabel("Layer Index")
